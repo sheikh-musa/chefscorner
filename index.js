9436ac6c2cafda7514219d6ab08c1f14db8ae0a1
sheikh-musa <sheikh.musa@outlook.com>
2022-02-24 02:20:49 +0800

    wip

--- index.js
+++ index.js
@@ -1,79 +1,35 @@
-// When the user scrolls the page, execute myFunction
-// window.onscroll = function () {
-// 	stickyNav();
-// };
+// When the user scrolls the page, execute stickyNav
+window.onscroll = function () {
+	stickyNav();
+};
 
-// // Get the navbar
-// const navbar = document.getElementById("navbar");
-// const content = document.getElementById("content");
-// // console.log(main);
+// Get the navbar
+const navbar = document.getElementById("navbar");
+const content = document.getElementById("content");
+// console.log(main);
 
-// // Get the offset position of the navbar
-// const sticky = navbar.offsetTop;
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+console.log(sticky);
 
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function stickyNav() {
-// 	if (window.pageYOffset >= sticky) {
-// 		navbar.classList.add("sticky");
-// 		content.classList.add("sticky-content");
-// 	} else {
-// 		navbar.classList.remove("sticky");
-// 		content.classList.remove("sticky-content");
-// 	}
-// }
-
-const mandi = [
-	{
-		item: "1/4 chicken mandi",
-		price: 6.5,
-	},
-	{
-		item: "1/2 chicken mandi",
-		price: 8.5,
-	},
-	{
-		item: "lamb shank mandi",
-		price: 16.5,
-	},
-];
-
-const rice = [
-	{
-		item: "kampong fried rice",
-		price: 6.5,
-	},
-	{
-		item: "seafood fried rice",
-		price: 6.5,
-	},
-];
-
-const noodles = [
-	{
-		item: "seafood hor fun",
-		price: 7.5,
-	},
-	{
-		item: "beef hor fun",
-		price: 8.5,
-	},
-	{
-		item: "laksa johore",
-		price: 8.5,
-	},
-	{
-		item: "hokkien mee",
-		price: 7.5,
-	},
-];
-
-// const menu = [mandi, rice, noodles];
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+		content.classList.add("sticky-content");
+	} else {
+		navbar.classList.remove("sticky");
+		content.classList.remove("sticky-content");
+	}
+}
 
 const menu = {
+	count: 9,
 	categories: [
 		{
 			id: 1,
 			name: "mandi",
+			count: 3,
 			items: [
 				{
 					name: "1/4 chicken mandi",
@@ -98,6 +54,7 @@ const menu = {
 		{
 			id: 2,
 			name: "rice",
+			count: 2,
 			items: [
 				{
 					name: "kampong fried rice",
@@ -116,6 +73,7 @@ const menu = {
 		{
 			id: 3,
 			name: "noodles",
+			count: 4,
 			items: [
 				{
 					name: "seafood hor fun",
@@ -146,9 +104,8 @@ const menu = {
 	],
 };
 
-// console.log(menu.mandi.items[0].name, menu.mandi.items[0].price);
-for (category of menu.categories) {
-	for (item of category.items) {
-		console.log(item.category, item.name, item.price);
-	}
-}
+// for (category of menu.categories) {
+// 	for (item of category.items) {
+// 		console.log(item.category, item.name, item.price);
+// 	}
+// }
